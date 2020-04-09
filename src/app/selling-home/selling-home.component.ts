1e578b7af8e8d2602103d417850039c931f7d9ef
BVaihinger <bvaihinger@colgate.edu>
2020-04-08 22:13:50 -0400

    Creation of listings

--- src/app/selling-home/selling-home.component.ts
+++ src/app/selling-home/selling-home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-selling-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellingHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  createListing(){
+    this.router.navigateByUrl('selling');
   }
 
 }
